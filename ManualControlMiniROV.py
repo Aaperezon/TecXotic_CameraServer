9c72a2bf04ddc27c03ef47475ef975de77e9529b
Aaperezon <aaron-po@hotmail.co.uk>
2021-07-12 23:36:49 +0000

    MiniROV controller added

--- ManualControlMiniROV.py
+++ ManualControlMiniROV.py
@@ -1,72 +1,18 @@
-import RPi.GPIO as GPIO          
-from time import sleep
+import HBridge
 
-in1 = 26
-in2 = 19
-in3 = 13
-in4 = 6
-
-en1 = 21
-en2 = 20
-temp1=1
-GPIO.setwarnings(False)
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(en1,GPIO.OUT)
-GPIO.setup(en2,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-GPIO.output(in3,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-motor1=GPIO.PWM(en1,1000)
-motor2=GPIO.PWM(en2,1000)
-motor1.start(0)
-motor2.start(0)
+left_motor = HBridge.HBridge(13,19,26)
+right_motor = HBridge.HBridge(16,20,21)
 
 def MoveMiniROV(direction):
-    if(direction == "f"):
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.HIGH)
-    elif(direction == "b"):
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in3,GPIO.HIGH)
-        GPIO.output(in4,GPIO.LOW)
-    elif(direction == "l"):
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.HIGH)
-    elif(direction == "r"):
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
-    else:
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
-
-def SetSpeed(speed):
-    motor1.ChangeDutyCycle(speed)
-    motor2.ChangeDutyCycle(speed)
-SetSpeed(50)
-
-
-if __name__ == "__main__":
-    try:
-        SetSpeed(50)
-        while True:
-            entrada = str(input("direccion: "))
-            MoveMiniROV(entrada)
-    except Exception as e:
-        print(e)
-    
-
+	if a == "f":
+		left_motor.Forward()
+		right_motor_Forward()
+	elif a == "b":
+		left_motor.Backward()
+		right_motor.Backward()
+	elif a == "l":
+		left_motor.Stop()
+		right_motor.Forward()
+	elif a == "r":
+		left_motor.Forward()
+		right_motor.Stop()
